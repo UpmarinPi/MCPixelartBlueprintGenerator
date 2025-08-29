007e5a598b9bd22617a7c4650763d1dcc2da8502
MDaiking <mdaiking117@gmail.com>
2025-08-29 23:35:52 +0900

    開発環境決定、諸々の環境構築

--- Main.py
+++
@@ -1,6 +0,0 @@
-# (input path: string) (output mode: int or string) (output path: string) (detail mode: 0 or 1)
-
-#input path: 初期状態の画像
-#output mode: 出力のモード
-#output path: 出力するフォルダ位置。初期値は"./Outputs"
-#detail mode: 詳細モードをtrueとするか。初期値はfalse。true時は色の数に 明・暗 が追加され、それらを指定する追加ファイル or 表示形式になる
---
+++ src/ts/index.ts
@@ -0,0 +1,7 @@
+interface Person {
+    name: string;
+    age: number;
+}
+
+const bob: Person = { name: "Bob", age: 25 };
+console.log(bob);
